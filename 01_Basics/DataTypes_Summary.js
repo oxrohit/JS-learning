27c2b6013f907d19119f32f94728a9d39d232a64
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-04-30 13:01:02 +0000

    Memory

--- 01_Basics/DataTypes_Summary.js
+++ 01_Basics/DataTypes_Summary.js
@@ -9,3 +9,26 @@
 
 */
 
+
+
+// +++++++++++++++++++++++++++++++++++++++++++ MEMOERY ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack => Primitive 
+
+let myName = "Rohit"
+let myLastName = "kumar"
+myLastName = " singh"
+console.log(myName + myLastName);
+
+// Heap => Non-Primitive
+
+let userOne = {
+    id : 100,
+    email : "User1@gmail.com",
+}
+
+let userTwo = userOne
+userOne.email = "UserTwo@gmail.com"
+
+console.log(userTwo.email);
+console.log(userOne.email);

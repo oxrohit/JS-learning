ecdc74096156d8abe9343241e9a675e0328cba2d
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-04-30 16:48:49 +0000

    Math

---
+++ 01_Basics/06_Maths.js
@@ -0,0 +1,26 @@
+const num = 100;
+// console.log(num);
+
+const score = new Number(200)
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const otherNumber = 1122.8767
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.pow(2,4));
+
+// console.log(Math.max(22,33,4,56,78));
+// console.log(Math.min(22,33,4,56,78));
+
+console.log(Math.random());

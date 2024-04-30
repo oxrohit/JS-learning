2fc269315364a1c6de349ed34001c83c8e71a229
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-04-30 06:48:06 +0000

    datatypes

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+/*
+Premetivie DataTypes
+
+number = 2^54
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+
+object
+
+*/
+
+let name = "Rohit"
+let age = 18
+let userExist = true
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof userExist)
+
+console.log(typeof undefined)
+// console.log(typeof BigInt)

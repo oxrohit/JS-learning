46a844388e338da353059d80aea4cb108e232a5d
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-04-30 16:54:01 +0000

    Math new

--- 01_Basics/06_Maths.js
+++ 01_Basics/06_Maths.js
@@ -23,4 +23,12 @@ const hundreds = 10000000
 // console.log(Math.max(22,33,4,56,78));
 // console.log(Math.min(22,33,4,56,78));
 
-console.log(Math.random());
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));

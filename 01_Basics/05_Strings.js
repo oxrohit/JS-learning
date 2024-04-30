7b935291e9edae89876e4dfc538e0d3a9932ae9b
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-04-30 13:31:54 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,12 @@
+let name = "Rohit"
+let age = 18
+
+console.log(`Hello i'm ${name} and i'm ${age} years old.`);
+
+//new way to declare Strings
+const gameName = new String('GtaV and RDR2')
+console.log(gameName);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());

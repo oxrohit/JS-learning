b2de80e61189c12c3bc3eb17c776f11ec54c214f
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-01 06:12:39 +0000

    New Day

---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,17 @@
+// DATES
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let newDate = new Date(2024, 2, 30)
+// let newDate = new Date(2024, 2, 30, 5, 6)
+let newDate = new Date("03-21-2000") // MM-DD-YYYY
+// console.log(newDate.toLocaleString());
+
+let myTimeStamp = new Date
+console.log(myTimeStamp.getMonth());
+console.log(myTimeStamp.getMinutes());
+console.log(myTimeStamp.getDay());
+console.log(myTimeStamp.getFullYear());

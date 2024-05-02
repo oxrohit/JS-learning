15cb985191c10fb18a2cfc764e487694c4c31f80
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-02 12:46:56 +0000

    iife

---
+++ 03_Basics/05_IIFE.js
@@ -0,0 +1,16 @@
+//          IIFE (Immediately Invoked Function Expression) 
+
+( function firstName(params) {
+ 
+   console.log("Rohit"); 
+})();
+
+((lastName) => {
+        console.log(`Kumar ${lastName}`);
+})("Singh");
+
+
+// ( function lastName(params) {
+ 
+//    console.log("Singh"); 
+// })();

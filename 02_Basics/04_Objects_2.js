c3dea9fc3b70455ecdbc43625396696ba45159d6
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-02 04:02:54 +0000

    Objects

--- 02_Basics/04_Objects_2.js
+++ 02_Basics/04_Objects_2.js
@@ -18,4 +18,38 @@ const newUser = {
     }
 }
 
-console.log(newUser.userName.UserFullName);
+// console.log(newUser.userName.UserFullName);
+
+// Merging Two or more Objects
+
+const obj1 = {1 : "a", 2 : "b"}
+const obj2 = {3 : "c", 4 : "d"}
+const obj3 = {5 : "e", 6 : "f"}
+
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+const obj4 = {...obj1, ...obj2, ...obj3}
+
+// console.log(obj4);
+
+const users = [
+    {
+        id : 9878,
+        name : "Rohit",
+        email : "Rohit@gmail.com"
+    },
+    {
+        id : 9879,
+        name : "Mohit", 
+        email : "Mohit@gmail.com"
+    }
+]
+
+// console.log( users[0].name);
+
+
+
+console.log(redditUser);
+console.log(Object.keys(redditUser));
+console.log(Object.values(redditUser));
+console.log(Object.entries(redditUser));
+console.log(redditUser.hasOwnProperty('add'));

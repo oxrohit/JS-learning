617dd20dc81a7dd2551254cc8514be0f462199db
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-03 12:25:45 +0000

    loops

---
+++ 05_Loops/02_while.js
@@ -0,0 +1,25 @@
+let i = 0
+
+// while (i <= 10) {
+//     console.log(`Value is ${i}`);
+
+//     i = i + 2
+// }
+
+let myArray = [1, 2,3 ,4,5,6, "Rohit"]
+let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+// do-while loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);

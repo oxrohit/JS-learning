c8c2e81958758ed4fd2e9d31ee372e5e9233bd75
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-03 08:27:51 +0000

    loops

---
+++ 05_Loops/01_loops.js
@@ -0,0 +1,27 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    // console.log(element);
+}
+
+// NESTED LOOP
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Table : ${i}`);
+    
+    
+    for (let j = 1; j <= 10; j++) {
+        
+        // console.log( i + '*' + j + '=' + i*j);
+    }
+    // console.log();
+}
+
+
+let myarr = [1,2,3,4,5,6,7]
+console.log(myarr.length);
+
+for (let index = 0; index < myarr.length; index++) {
+    const element = myarr[index];
+    console.log(element);
+    
+}

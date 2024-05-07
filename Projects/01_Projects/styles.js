6a5d58b2b8576f7e27cdc9f499260c928652a249
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-07 12:23:05 +0000

    Project ONE

---
+++ Projects/01_Projects/styles.js
@@ -0,0 +1,34 @@
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]       
+    }
+    return color 
+}
+
+// console.log(randomColor());
+let IntervalID;
+
+const startChaningColor = function () {
+    if (!IntervalID) {
+        IntervalID = setInterval(changeBgColor, 1000)
+    }
+        
+    function changeBgColor(){
+    document.body.style.backgroundColor = randomColor();
+    };
+}
+
+const stopChaningColor = function () {
+    clearInterval(IntervalID);
+    IntervalID = null;
+}
+
+document.querySelector('#start').
+addEventListener('click', startChaningColor)
+
+
+document.querySelector('#stop').
+addEventListener('click', stopChaningColor)
+

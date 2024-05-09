23e059b224bb80474d0c5b8ed0a3302c46896f3b
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-09 04:29:28 +0000

    Stop Watch

---
+++ Projects/StopWatch/script.js
@@ -0,0 +1,4 @@
+const display = document.getElementById('display')
+const startBtn = document.getElementById('startbtn')
+const stopBtn = document.getElementById('stopbtn')
+const resetBtn = document.getElementById('resetbtn')

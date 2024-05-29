508942b7fa54caccd9794331f72dc4b8e95cc812
oxrohit <83065022+oxrohit@users.noreply.github.com>
2024-05-29 14:55:39 +0000

    Tic Tac Toe Game

---
+++ Projects/Tic_Tac_Toe/script.js
@@ -0,0 +1,12 @@
+let currentPlayer = "X"
+let arr = Array(9).fill(null)
+
+
+
+function handleClick(el){
+    const id = Number(el.id);
+    arr[id] = currentPlayer
+    el.innerText = currentPlayer
+    currentPlayer = currentPlayer == "X" ? "O" : "X"
+    console.log(arr);
+}
